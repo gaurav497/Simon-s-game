0a76a2e7f711bc16b0a93eca8bb751b574651cbd
gaurav497 <grvsingh497@gmail.com>
2023-12-07 23:19:47 +0530

    Initital commit

---
+++ index.js
@@ -0,0 +1,71 @@
+var userClickedPattern=[];
+var buttonColor=["red","blue","green","yellow"];
+var gamePattern=[];
+var level=0;
+function checkAnswer() {
+    if(userClickedPattern[userClickedPattern.length-1]===gamePattern[userClickedPattern.length-1]){
+            console.log("success");
+            if(userClickedPattern.length==gamePattern.length){
+                setTimeout(() => {
+                   newSequence(); 
+                 }, 1000);
+            }
+    }else{
+        playSound("wrong");
+        $("body").addClass("game-over");
+        setTimeout(()=>{
+            $("body").removeClass("game-over");
+        },200);
+        $("h2").text("Game Over, Press Any Key to Restart!");
+       startOver();
+        console.log("wrong");
+    }
+  }
+  function startOver(){
+level=0;
+gamePattern=[];
+start=false;
+  }
+function animatePress(currentColor){
+    console.log(currentColor);
+    $("."+currentColor).addClass("pressed");
+    setTimeout(() => {
+       $("."+currentColor).removeClass("pressed"); 
+    }, 100);
+}
+function playSound(randomChoosenColor){
+    var audio = new Audio('sounds\\'+randomChoosenColor+'.mp3');
+    audio.play();
+}
+function newSequence() {
+    userClickedPattern = []; 
+var randomNumber=Math.floor(Math.random()*4);
+ 
+ var randomChoosenColor=buttonColor[randomNumber];
+ gamePattern.push(randomChoosenColor);
+ console.log(randomChoosenColor);
+
+ $("#"+randomChoosenColor).fadeOut();
+ $("#"+randomChoosenColor).fadeIn();
+playSound(randomChoosenColor);
+$("h2").text("Level "+level);
+level++;
+checkAnswer(level);
+}
+
+$(".btn").click(function(event){
+    var userChoosenColor=$(this).attr("id");
+    userClickedPattern.push(userChoosenColor);
+    playSound(userChoosenColor);
+    animatePress(userChoosenColor);
+   checkAnswer();
+});
+var start=false;
+$(document).keydown(function (e) {
+    
+        if(start==false){
+            start=true;
+        newSequence();
+        }
+    
+});
